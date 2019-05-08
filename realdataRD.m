02c2cc72c0ec06d108851cab5a64e798d065ba6c
Eric <543709852@qq.com>
2019-05-08 11:10:42 +0800

    final

--- realdataRD.m
+++ realdataRD.m
@@ -1,6 +1,5 @@
-%% Traditional RDA imaging algorithm with CD data by XuXinbo in 2018.1
 clear all;
-close all;
+%close all;
 %% 物理常量
 c = 2.9979e+8; 
 %% 雷达参数
@@ -16,34 +15,26 @@ kd = 2*vm^2*mradar.fc/c/r0
 %% 回波数据（已去载频）
 CDdata = load('CDdata1.mat');
 sr=CDdata.data;
-%% 匹配滤波
 [N,M] = size(sr);
-m=1:1:M;
-t=(-M/2+m)./mradar.fr;
-sref=exp(-1j*pi*mradar.kr*t.^2);
+%% 匹配滤波
+t = 0:1/mradar.fr:mradar.tr;
+sref = exp(-1j*pi*mradar.kr*t.^2);
 result = matchfilter(sr,sref,0);
-figure,imagesc(abs(result))
+figure,imagesc(abs(result));
+%graymap = gray; colormap(graymap(end:-1:1,:));
 %% 方位向匹配滤波
-% srf = exp(-2*pi*1j*0.5*kd*srftm.^2+2*pi*1j*fdc*srftm);
-% HRRPSZ = [zeros(floor(fsfd*acclong/2),size(HRRPSout,2));HRRPSout];
-% sarimage = matchfilter(HRRPSZ,srf,1);
-%  figure;plot(1/fsfd*(1:size(sarimage,1))*vm,abs(sarimage(:,432)))
-%  sarscreen = sarimage(1:size(HRRPS,1),:);
-%  figure;imagesc(c*t(1:size(HRRPSout,2))/2,tmf,abs(HRRPSout));xlabel('距离/m');ylabel('tm');
-% figure;imagesc(c*t(1:size(HRRPS,2))/2,1/fsfd*(1:size(sarscreen,1))*vm,abs(sarscreen));xlabel('距离/m');ylabel('方位/m');
-%---------------azimuth compression
 n=1:1:N;
 tm=(-N/2+n)./mradar.PRF;
-delta = c/2/mradar.fr;
-ka=2*vm^2*mradar.fc/c/(r0+(1024+970-1)*delta);
-% ref2=exp(-1j*pi*ka*tm.^2);
-% REF2=conj(fftshift(fft(ref2,N)).*hamming(N).');   %用时域匹配函数FFT匹配滤波最终结果不对
-% azimuth=iftx(ftx(range).*(REF2.'*ones(1,M)));
+%%在时域操作，斜视角未知，多普勒中心频率未知，无法时域操作
+% sref2=exp(1j*pi*kd*tm.^2);
+% azimuth= matchfilter(result,sref2,1);
+%%在频域操作，使频域函数覆盖整个频率范围，从而覆盖实际信号所在
 fa=-mradar.PRF/2+mradar.PRF*(n-1)/N;
 ref2=exp(-1j*pi*fa.^2./kd);
-azimuth=ifft(fft(result).*(ref2.'*ones(1,M)));       %直接写出频域形式结果正确
-% azimuth=ifft(fft(range).*(REF2.'*ones(1,M)));
+azimuth=ifft(fft(result).*(ref2.'*ones(1,M)));       
+%% 成像结果
 image=mat2gray(abs(azimuth));
 image=image*8*1;
-imwrite(image,'SAR_test.jpg','JPG');
-figure,imagesc(abs(azimuth))
+imwrite(image,'SAR_RD.jpg','JPG');
+figure,imagesc(abs(azimuth));
+%graymap = gray;colormap(graymap(end:-1:1,:));
